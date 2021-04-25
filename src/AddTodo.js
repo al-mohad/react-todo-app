2639bb08718c9b82bf74b7121a79b376a06deea0
Muhammad Buhari <gmbdairy@gmail.com>
2021-04-25 23:14:43 +0100

    done

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -14,13 +14,15 @@ class AddTodo extends Component{
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.addTodo(this.state);
+        this.setState({content: ''})
     }
+
     render(){
         return(
             <div className='form'>
                 <form onSubmit={this.handleSubmit}>
                     <label>Add new todo</label>
-                    <input type='text' onChange={this.handleChange} />
+                    <input type='text' onChange={this.handleChange} value={this.state.content}/>
                 </form>
             </div>
         );
